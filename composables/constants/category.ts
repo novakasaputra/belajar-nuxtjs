e7c64096b539dae3edd8f7f7033eeb8de8a07ff4
Novaka <vakaganteng@gmail.com>
2023-10-26 17:01:28 +0700

    product

---
+++ composables/constants/category.ts
@@ -0,0 +1,22 @@
+export const category: Category[] = [
+    {
+    id: 1,
+    name: "Baju",
+    },
+    {
+    id: 2,
+    name: "Jam",
+    },
+    {
+    id: 3,
+    name: "Sepatu",
+    },
+    {
+    id: 4,
+    name: "Celana",
+    },
+    ]
+    export interface Category {
+    id? : number,
+    name?: string,
+}
